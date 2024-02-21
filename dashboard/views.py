a566e62d3b6b30c3a21587db4ac45992cf990f2d
Peter Garvanski <garvanski.peter@gmail.com>
2024-02-21 17:09:13 +0000

    feat: added settings url and view

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-
+@login_required
 def dashboard(request):
     """
     A view to return the dashboard page of the website.
@@ -10,3 +11,9 @@ def dashboard(request):
         "fav_tickers" : ["BTC-USD", "ETH-USD", "SOL-USD", "XRP-USD", "ADA-USD","DOT-USD", "USDT-USD", "LINK-USD"],
     }
     return render(request, 'dashboard/dashboard.html', context)
+
+def settings(request):
+    """
+    A view to return the Settings page of the website.
+    """
+    return render(request, 'dashboard/settings.html')
