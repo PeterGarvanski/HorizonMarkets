47d5484ee9130d8bca20d691a835e208d460fa2f
Peter Garvanski <garvanski.peter@gmail.com>
2024-03-03 18:56:37 +0000

    feat: completed data integration with websocket for up to 8 symbols.

--- dashboard/static/dashboard/js/binance-market-data.js
+++ dashboard/static/dashboard/js/binance-market-data.js
@@ -3,51 +3,59 @@ const dataElement = document.getElementsByClassName('fav-tickers')[0];
 const cryptoList = dataElement.getAttribute('data');
 const modifiedCryptoList = cryptoList.replace(/'/g, '"');
 const favTickers = JSON.parse(modifiedCryptoList);
-const firstHalf = favTickers.slice(0, 4);
 
+// Connecting to Binance Websocket
 const websocketEndpoint = 'wss://stream.binance.com:9443/ws';
 const ws = new WebSocket(websocketEndpoint);
 
-// Function to subscribe to the WebSocket stream
-const getTickerPrices = (tickers) => {
+// Keep Track of time, incase of need to throtle
+let lastUpdateTimestamp = 0;
 
-    // Subscribing to ticker streams for the provided symbols
+// Function to subscribe to the WebSocket stream
+const subscribeToTicker = (ticker) => {
+    const ws = new WebSocket(websocketEndpoint); // Create a new WebSocket connection
+
+    // Subscribing to the ticker stream
     ws.onopen = () => {
-        tickers.forEach(ticker => {
-            const streamName = `${ticker.toLowerCase()}usdt@ticker`;
-            const subscriptionMsg = JSON.stringify({
-                method: 'SUBSCRIBE',
-                params: [streamName],
-                id: 1
-            });
-            ws.send(subscriptionMsg);
+        const streamName = `${ticker.toLowerCase()}usdt@ticker`;
+        const subscriptionMsg = JSON.stringify({
+            method: 'SUBSCRIBE',
+            params: [streamName],
+            id: 1
         });
+        ws.send(subscriptionMsg);
     };
 
-    // Handling incoming data
+    // Handling incoming data, and updating Elements
     ws.onmessage = (event) => {
-        const tickerData = JSON.parse(event.data);
+        const currentTime = Date.now();
+        if (currentTime - lastUpdateTimestamp >= 0) {
+            const tickerData = JSON.parse(event.data);
+            console.log(tickerData);
+            
+            if (tickerData.e === '24hrTicker') {
+                const tickerValueElement = document.getElementById(`${tickerData.s}_value`);
+                const tickerPercentageElement = document.getElementById(`${tickerData.s}_percentage`);
 
-        if (tickerData.e === '24hrTicker') {
-            const tickerValueElement = document.getElementById(`${tickerData.s}_value`);
-            const tickerPercentageElement = document.getElementById(`${tickerData.s}_percentage`);
+                const tickerPrice = parseFloat(tickerData.c);
+                const tickerPercentage = parseFloat(tickerData.P);
 
-            const tickerPrice = parseFloat(tickerData.c);
-            const tickerPercentage = parseFloat(tickerData.P);
+                if (tickerPercentage > 0) {
+                    tickerPercentageElement.style.backgroundColor = "#11a452";
+                    
+                } else if (tickerPercentage < 0) {
+                    tickerPercentageElement.style.backgroundColor = "#ef415b";
 
-            if (tickerPercentage > 0) {
-                tickerPercentageElement.style.backgroundColor = "#11a452";
-                
-            } else if (tickerPercentage < 0) {
-                tickerPercentageElement.style.backgroundColor = "#ef415b";
+                } else {
+                    tickerPercentageElement.style.backgroundColor = "#93b8e1";
+                }
 
-            } else {
-                tickerPercentageElement.style.backgroundColor = "#93b8e1";
+                tickerValueElement.innerHTML = tickerPrice.toFixed(2);
+                tickerPercentageElement.innerHTML = tickerPercentage.toFixed(2) + "%";
+
+                lastUpdateTimestamp = currentTime;
             }
-
-            tickerValueElement.innerHTML = tickerPrice.toFixed(2);
-            tickerPercentageElement.innerHTML = tickerPercentage.toFixed(2) + "%";
-        };
+        }
     };
 
     // Handling errors
@@ -56,5 +64,12 @@ const getTickerPrices = (tickers) => {
     };
 };
 
+// Function to subscribe to tickers using multiple WebSocket connections
+const subscribeToTickers = (tickers) => {
+    tickers.forEach(ticker => {
+        subscribeToTicker(ticker);
+    });
+};
+
 // Calling the function to subscribe to ticker streams
-getTickerPrices(firstHalf);
+subscribeToTickers(favTickers);
--- dashboard/views.py
+++ dashboard/views.py
@@ -17,10 +17,12 @@ def dashboard(request):
     context = {
         "account_balance" : user_profile.account_balance,
         "account_history" : "50000,45789,36789,42516,52456,59123,61123,65930,68234,70123,67282,65929",
-        "fav_tickers" : ["BTC", "ETH", "SOL", "XRP", "ADA","DOT", "USDT", "LINK"],
+        "fav_tickers" : ["BTC", "ETH", "BNB", "SOL", "USDC", "ADA", "AVAX", "DOT"],
         "stripe_pk" : "pk_test_51OiF1tJWloYFxaUMwarc2ylIFT2HDdMBdhIoQ90gX5ys75vKPeH14zg1rs4dMriikfWXMgxMa429xi22q4tvVhi200Ckh9XClc",
         "stripe_sk" : "sk_test_51OiF1tJWloYFxaUMA4g6SWVtSdaTb0x4wMTmnSX1KxMMwxOGKbaOQkvSbMTMScTqnIKm9Hgq4GnVDW5X6o6Wr00j00vJKwFWp0",
     }
+
+    # , "ADA", "AVAX", "DOT"
 
     return render(request, 'dashboard/dashboard.html', context)
 
