b5d674ef724d1e762bf66ab869a146f22c8315e6
Peter Garvanski <garvanski.peter@gmail.com>
2024-03-28 16:43:59 +0000

    feat: added more functionality to web socket

--- trade/trade.py
+++ trade/trade.py
@@ -1,5 +1,4 @@
 import websocket
-import base64
 import hashlib
 import hmac
 import json
@@ -7,100 +6,118 @@ import time
 import uuid
 from cryptography.hazmat.primitives.serialization import load_pem_private_key
 
-API_KEY = 'pd50R4mhRgsnBy4rHmUEG5ZxIjIMQsOh8aclJxjpTYCAehNEIDMfYWGIUkSjNxHA'
-SECRET_KEY = 'vYiipCL4c1bK8MrvcpXnYhGVOyabupuByI83oItfXdBhbDjbwFBshNECdCfxvBoN'
+
+API_KEY = 'eVCKz8VvUi2FCSlxbuV3LT54cWVPa4OVU6ycVVEu0AyBXUHbG4RJmTNfxnqXaBPQ'
+SECRET_KEY = 'kR9m1gC574eV5SUz1LrSGpQ1ul0kDi3NF3PrKHaCmcqY0wIw2HbY390E1l9YvBRy'
 
 
 def construct_signature_payload(params):
     sorted_params = sorted(params.items(), key=lambda x: x[0])
-    return '&'.join([f"{param}={value}" for param, value in sorted_params])
+    return '&'.join([f'{param}={value}' for param, value in sorted_params])
 
 
 def compute_signature(payload, secret_key):
     return hmac.new(secret_key.encode('utf-8'), payload.encode('utf-8'), hashlib.sha256).hexdigest()
 
 
-def place_order_message(ws, message):
-    print("Received message:", message)
+def trade_on_message(ws, response):
+    print('Received message:', response)
+
+    if response.get('status') == 'FILLED':
+        print(True)
+    
+    ws.close()
 
 
-def query_order_message(ws, message):
-    print("Received message:", message)
-
-
-def place_order_open(ws):
-    print("Order webSocket connection opened")
-
-    # Set up the request parameters
-    order = {
-        'id': str(uuid.uuid4()),
-        'method': 'order.place',
-        'params' : {
-            'symbol': 'BTCUSDT',
-            'side': 'BUY',
-            'type': 'MARKET',
-            'quantity': '0.1',
-            'timestamp': int(time.time() * 1000),
-            'apiKey': API_KEY,
-        }
-    }
+def trade_on_open(ws, params):
+    print('Order webSocket connection opened')
 
     # Sign the request
-    payload = construct_signature_payload(order['params'])
+    payload = construct_signature_payload(params['params'])
     signature = compute_signature(payload, SECRET_KEY)
-    order['params']['signature'] = signature
+    params['params']['signature'] = signature
 
     # Send the order message
-    ws.send(json.dumps(order))
+    ws.send(json.dumps(params))
 
 
-def query_order_open(ws):
-    print("Query webSocket connection opened")
+def trade_on_close(ws):
+    print('Order webSocket connection closed')
 
-    # Set up the request parameters
-    order = {
-        'id': str(uuid.uuid4()),
-        'method': 'order.status',
-        'params' : {
-            'symbol': 'BTCUSDT',
-            'orderId': '7329516',
-            'timestamp': int(time.time() * 1000),
-            'apiKey': API_KEY,
+
+def handle_trade(params, symbol, side, quantity, price, take_profit, stop_loss):
+
+    if params == 'MARKET':
+        request_message = {
+            'id': str(uuid.uuid4()),
+            'method': 'order.place',
+            'params' : {
+                'symbol': symbol,
+                'side': side,
+                'type': 'MARKET',
+                'quantity': quantity,
+                'timestamp': int(time.time() * 1000),
+                'apiKey': API_KEY,
+            }
         }
-    }
 
-    # Sign the request
-    payload = construct_signature_payload(order['params'])
-    signature = compute_signature(payload, SECRET_KEY)
-    order['params']['signature'] = signature
+    elif params == 'LIMIT':
+        request_message = {
+            'id': str(uuid.uuid4()),
+            'method': 'order.place',
+            'params' : {
+                'symbol': symbol,
+                'side': side,
+                'type': 'LIMIT',
+                'price': price,
+                'quantity': quantity,
+                'timeInForce': 'GTC',
+                'timestamp': int(time.time() * 1000),
+                'apiKey': API_KEY,
+            }
+        }
 
-    # Send the order message
-    ws.send(json.dumps(order))
+    elif params == 'OPEN_ORDERS':
+        request_message = {
+            'id': str(uuid.uuid4()),
+            'method': 'openOrders.status',
+            'params': {
+                'symbol': symbol,
+                'timestamp': int(time.time() * 1000),
+                'apiKey': API_KEY,
+            }
+        }
 
+    elif params == 'CANCEL_OPEN_ORDERS':
+        request_message = {
+            'id': str(uuid.uuid4()),
+            'method': 'openOrders.cancelAll',
+            'params': {
+                'symbol': symbol,
+                'timestamp': int(time.time() * 1000),
+                'apiKey': API_KEY,
+            }
+        }
 
-def place_order_close(ws):
-    print("Order webSocket connection closed")
+    elif params == 'ORDER_STATUS':
+        request_message = {
+            'id': str(uuid.uuid4()),
+            'method': 'order.status',
+            'params': {
+                'symbol': symbol,
+                'orderId': '3090331',
+                'timestamp': int(time.time() * 1000),
+                'apiKey': API_KEY,
+            }
+        }
 
-
-def query_order_close(ws):
-    print("Query webSocket connection closed")
-
-
-def place_order():
     # Create a WebSocket connection
-    ws = websocket.WebSocketApp("wss://testnet.binance.vision/ws-api/v3",
-                                on_message=place_order_message,
-                                on_open=place_order_open,
-                                on_close=place_order_close)
+    ws = websocket.WebSocketApp('wss://testnet.binance.vision/ws-api/v3',
+                                on_message=trade_on_message,
+                                on_open=lambda ws: trade_on_open(ws, request_message),
+                                on_close=trade_on_close)
 
     ws.run_forever()
 
 
-def query_order():
-    # Create a WebSocket connection
-    ws = websocket.WebSocketApp("wss://testnet.binance.vision/ws-api/v3",
-                                on_message=query_order_message,
-                                on_open=query_order_open,
-                                on_close=query_order_close)
-
-    ws.run_forever()
+handle_trade('MARKET', 'BNBUSDT', 'BUY', '1', '', '', '')
